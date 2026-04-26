b6cfd91492ce83e1c57477556c8a3d03adf185a4
Ari Gardner <arigardner11@gmail.com>
2026-04-26 11:51:58 -0600

    building add handler

--- src/handlers.rs
+++ src/handlers.rs
@@ -8,3 +8,67 @@ probably will have something like
 
 definitely a couple others but this is the idea
 */
+
+use axum::extract::{State, Query};
+use axum::response::{Html, Redirect};
+use serde::Deserialize;
+use rand::random;
+
+use crate::AppState;
+use crate::model::{CustomerOrder, Roast};
+//use crate::parse::{parse_coffee, parse_size};
+
+
+
+/*
+these struct will allow us to handle the parameters we want by passingh them when we use a handler
+this will be used for example when using the basic get/
+
+will be passing these and the Appstate so we have access to cart, inventory and db
+*/
+#[derive(Deserialize)]
+pub struct PageParameters{
+    pub cart_id : Option<u32>
+}
+
+#[derive(Deserialize)]
+pub struct AddOrderParams{
+    pub cart_id : u32,
+    pub coffee : String,
+    pub size : String,
+    pub quantity : f32
+}
+
+
+pub struct AddInventoryParams{
+    pub cart_id : Option<u32>
+}
+
+
+pub struct CheckoutParams{
+    pub cart_id : u32
+}
+
+/*
+gonna try to build a handler for when the user clicks a add item to order
+before doing all the setup of the actual page
+*/
+
+
+pub async fn addItem(State(state) : State<AppState>, Query(params) : Query<AddOrderParams>) -> Redirect{
+
+    let coffee = &params.coffee;
+    let size = &params.size;
+
+    //believe using .lock() here is right because we dont want multiple threads editing
+    let mut carts = state.carts.lock().unwrap();
+    let mut inventory = state.inventory.lock().unwrap();
+
+
+    //finding the entry in the hashmap and if it doesnt exist we insert the a customer order with the current cart_id
+    let cart = carts
+        .entry(params.cart_id)
+        .or_insert(||CustomerOrder::new(params.cart_id as i32));
+
+}
+
